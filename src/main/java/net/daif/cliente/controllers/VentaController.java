1e2006569f9bcc165d7fd3c01d8c9396e4188c2d
DaifMX <applediegoaif@gmail.com>
2022-11-11 04:56:01 -0600

    GET Correction

--- src/main/java/net/daif/cliente/controllers/VentaController.java
+++ src/main/java/net/daif/cliente/controllers/VentaController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/venta")
@@ -15,9 +15,9 @@ public class VentaController {
     @Autowired
     VentaService ventaService;
 
-    @GetMapping()
-    public ArrayList<VentaModel> obtenerVenta(){
-        return ventaService.obtenerVenta();
+    @GetMapping("/get/all")
+    public ResponseEntity<List<VentaModel>> getAll(){
+        return new ResponseEntity<>(this.ventaService.getAll(), HttpStatus.OK);
     }
 
     @PostMapping("/save")
--- src/main/java/net/daif/cliente/services/VentaService.java
+++ src/main/java/net/daif/cliente/services/VentaService.java
@@ -6,18 +6,20 @@ import net.daif.cliente.repositories.VentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class VentaService {
     @Autowired
     VentaRepository ventaRepository;
 
-    public ArrayList<VentaModel> obtenerVenta(){
-        return (ArrayList<VentaModel>) ventaRepository.findAll();
+    public VentaModel guardarVenta(VentaModel venta){
+        venta.setFecha_alta(LocalDateTime.now());
+        return ventaRepository.save(venta);
     }
 
-    public VentaModel guardarVenta(VentaModel venta){
-        return ventaRepository.save(venta);
+    public List<VentaModel> getAll(){
+        return this.ventaRepository.findAll();
     }
 }
